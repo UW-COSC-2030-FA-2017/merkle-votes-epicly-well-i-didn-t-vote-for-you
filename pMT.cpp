5c3124b1e538db6906f16c3cbcf227ce6692d7fa
agiljum <31866792+agiljum@users.noreply.github.com>
2017-12-01 06:55:15 -0700

    Update pMT.cpp

--- pMT.cpp
+++ pMT.cpp
@@ -1,70 +1,86 @@
 #include "pMT.h"
+#include "bTREE.h"
 
 pMT::pMT(int hashSelect)
 /**
- * @brief 
- * @param hashSelect a number corresponding to the hashfunction to use for this pMT
- * @return 
- */
+* @brief
+* @param hashSelect a number corresponding to the hashfunction to use for this pMT
+* @return
+*/
 {
+	selectedHash = hashSelect;
+	hashS(hashSelect);
 }
 
 pMT::~pMT()
 /**
- * @brief destructor
- * @return nada
- */
+* @brief destructor
+* @return nada
+*/
 {
+}
+
+string pMT::hashS(string data)
+{
+	switch (selectedHash) {
+	case 1: return hash_1(data);
+		break;
+	case 2: return hash_2(data);
+		break;
+	case 3: return hash_3(data);
+		break;
+	default: return "---";
+	}
 }
 
 int pMT::insert(string vote, int time)
 /**
- * @brief insert a vote and time into a leaf node of tree
- * @param vote - a string
- * @param time - an int representing the time 
- * @return the number of operations needed to do the insert, -1 if out of memory
- */
-
+* @brief insert a vote and time into a leaf node of tree
+* @param vote - a string
+* @param time - an int representing the time
+* @return the number of operations needed to do the insert, -1 if out of memory
+*/
 {
+	
 }
 
 int pMT::find(string vote, int time, int hashSelect)
 /**
- * @brief given a vote, timestamp, and hash function, does this vote exist in the tree?
- * @param vote, a string
- * @param time, an int
- * @param hashSelect, an int corresponding to the hash functions _1, _2, and _3
- * @return 0 if not found, else number of opperations required to find the matching vote
- */
+* @brief given a vote, timestamp, and hash function, does this vote exist in the tree?
+* @param vote, a string
+* @param time, an int
+* @param hashSelect, an int corresponding to the hash functions _1, _2, and _3
+* @return 0 if not found, else number of opperations required to find the matching vote
+*/
 {
-    
+
 }
 
 int pMT::findHash(string mhash)
 /**
- * @brief does this hash exist in the tree?
- * @param mhash, a string to search for in the tree
- * @return 0 if not found, else number of opperations required to find the matching hash
- */
+* @brief does this hash exist in the tree?
+* @param mhash, a string to search for in the tree
+* @return 0 if not found, else number of opperations required to find the matching hash
+*/
 {
 }
 
 
 string pMT::locateData(string vote)
 /**
- * @brief Function takes a hash of Vote and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root. 
- * @param vote, the data to search for 
- * @return sequence of L's and R's comprising the movement to the leaf node; else return a dot '.'
- */
+* @brief Function takes a hash of Vote and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root.
+* @param vote, the data to search for
+* @return sequence of L's and R's comprising the movement to the leaf node; else return a dot '.'
+*/
 {
 }
 
 string pMT::locateHash(string mhash)
 /**
- * @brief Function takes a hash and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root. 
- * @param mhash, the hash to search for 
- * @return sequence of L's and R's comprising the movement to the hash node, ; else return a dot '.'
- */
+* @brief Function takes a hash and returns the sequence of (L)eft and (R)ight moves to get to that node starting from root.
+* @param mhash, the hash to search for
+* @return sequence of L's and R's comprising the movement to the hash node, ; else return a dot '.'
+*/
 {
 }
 
@@ -72,28 +88,28 @@ string pMT::locateHash(string mhash)
 
 string pMT::hash_1(string key)
 /**
- * @brief A function that takes in a key and returns a hash of that key using some custom function
- * @param key, a string
- * @return a hash of the key
- */
+* @brief A function that takes in a key and returns a hash of that key using some custom function
+* @param key, a string
+* @return a hash of the key
+*/
 {
-   unsigned int hash = 0;
+	unsigned int hash = 0;
 
-   for(std::size_t i = 0; i < key.length(); i++)
-   {
-      hash = (hash * i) + key[i];
-   }
-    //itoa (i,hash,16);
-    //hash = std::hex << hash;
-   return std::to_string(hash);
+	for (std::size_t i = 0; i < key.length(); i++)
+	{
+		hash = (hash * i) + key[i];
+	}
+	//itoa (i,hash,16);
+	//hash = std::hex << hash;
+	return std::to_string(hash);
 }
 
 string pMT::hash_2(string key)
 /**
- * @brief A function that takes in a key and returns a hash of that key using some custom function
- * @param key, a string
- * @return a hash of the key
- */
+* @brief A function that takes in a key and returns a hash of that key using some custom function
+* @param key, a string
+* @return a hash of the key
+*/
 {
 	unsigned int hash = 0b0000000000000001;
 	unsigned int thing = 0b0001010010100000;
@@ -107,63 +123,63 @@ string pMT::hash_2(string key)
 
 string pMT::hash_3(string key)
 /**
- * @brief A function that takes in a key and returns a hash of that key using some custom function
- * @param key, a string
- * @return a hash of the key
- */
+* @brief A function that takes in a key and returns a hash of that key using some custom function
+* @param key, a string
+* @return a hash of the key
+*/
+{
+	unsigned int hash = 1234;
+
+	for (int i = 0; i < key.length(); i++)
+	{
+		hash = ((hash << 5) + hash) + key[i];
+	}
+	return std::to_string(hash);
+}
+
+bool operator ==(const pMT& lhs, const pMT& rhs)
+/**
+* @brief Comparison between two merkle trees
+* @param lhs, the left hand side of the equality statment
+* @param rhs, the right hand side of the equality statement
+* @return true if equal, false otherwise
+*/
+{
+	// If root node hashes match, the trees are equal
+	if (lhs.root->root == rhs.root->data)
+		return true;
+	else
+		return false;
+}
+
+bool operator !=(const pMT& lhs, const pMT& rhs)
+/**
+* @brief Comparison between two merkle trees
+* @param lhs, the left hand side of the equality statment
+* @param rhs, the right hand side of the equality statement
+* @return true if not equal, false otherwise
+*/
+{
+
+}
+
+std::ostream& operator <<(std::ostream& out, const pMT& p)
+/**
+* @brief Print out a tree
+* @param out
+* @param p
+* @return a tree to the screen
+*/
 {
 }
 
-friend bool pMT::operator ==(const pMT& lhs, const pMT& rhs)
+
+pMT operator ^(const pMT& lhs, const pMT& rhs)
 /**
- * @brief Comparison between two merkle trees
- * @param lhs, the left hand side of the equality statment
- * @param rhs, the right hand side of the equality statement
- * @return true if equal, false otherwise
- */
-{
-}
-
-friend bool pMT::operator !=(const pMT& lhs, const pMT& rhs)
-/**
- * @brief Comparison between two merkle trees
- * @param lhs, the left hand side of the equality statment
- * @param rhs, the right hand side of the equality statement
- * @return true if not equal, false otherwise
- */
-{
-    
-}
-
-friend pMT pMT::operator ^=(const pMT& lhs, const pMT& rhs)
-/**
- * @brief XOR between two merkle trees
- * @param lhs, the left hand side of the equality statment
- * @param rhs, the right hand side of the equality statement
- * @return true if not equal, false otherwise
- */
-{
-    
-}
-
-
-friend std::ostream& pMT::operator <<(std::ostream& out, const pMT& p)
-/**
- * @brief Print out a tree
- * @param out
- * @param p
- * @return a tree to the screen
- */
-{
-}
-
-
-friend pMT pMT::operator ^(const pMT& lhs, const pMT& rhs)
-/**
- * @brief Where do two trees differ
- * @param lhs
- * @param rhs
- * @return a tree comprised of the right hand side tree nodes that are different from the left
- */
+* @brief Where do two trees differ
+* @param lhs
+* @param rhs
+* @return a tree comprised of the right hand side tree nodes that are different from the left
+*/
 {
 }
